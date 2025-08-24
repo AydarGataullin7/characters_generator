1200d0cc15ef467371eaa11237a4056a24a817a7
AydarGataullin7 <aydarcr7@gmail.com>
2025-08-25 01:35:54 +0500

    Add files via upload

---
+++ character_creator.py
@@ -0,0 +1,87 @@
+import os
+import random
+from faker import Faker
+import file_operations
+
+
+
+SKILLS = [
+    "Стремительный прыжок", "Электрический выстрел", "Ледяной удар",
+    "Стремительный удар", "Кислотный взгляд", "Тайный побег",
+    "Ледяной выстрел", "Огненный заряд"
+]
+
+LETTERS_MAPPING = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+OUTPUT_FOLDER = "results"
+TEMPLATE_FOLDER = "temp/charsheet.svg"
+
+
+def convert_skills_to_runic(skills_list, mapping):
+    runic_skills=[]
+    for skill in skills_list:
+        converted_skill=skill
+        for rus_letter, runic_letter in mapping.items():
+            converted_skill=converted_skill.replace(rus_letter, runic_letter)
+        runic_skills.append(converted_skill)
+    return runic_skills
+
+
+def generate_character_context(fake, runic_skills):
+    unique_skills=random.sample(runic_skills, 3)
+    return {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": unique_skills[0],
+        "skill_2": unique_skills[1],
+        "skill_3": unique_skills[2]
+    }
+
+
+def main():
+    runic_skills=convert_skills_to_runic(SKILLS, LETTERS_MAPPING)
+    
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    
+    fake=Faker("ru_RU")
+    
+    for i in range(10):
+        context=generate_character_context(fake, runic_skills)
+        
+        filename=os.path.join(OUTPUT_FOLDER, "character_{}.svg".format(i+1))
+        file_operations.render_template(TEMPLATE_FOLDER, filename, context)
+
+
+if __name__ == '__main__':
+    main()
